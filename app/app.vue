1c60e1979a663347391accf1063ceaf91c61488b
Gianni <gianni.cds@gmail.com>
2026-01-05 21:22:08 +0100

    Giorno 1: Aggiunta reattività e bottone interattivo

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,42 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>{{ titolo }}</h1>
+    <p>Benvenuto a bordo della tua nuova avventura.</p>
+     <button @click="cambiaTitolo">Cambia Stagione</button>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+// --- VECCHIO CODICE (Commentato, non si esegue) ---
+/*
+const titolo = ref("Titolo Vecchio")
+
+function cambiaTitolo() {
+  // Vecchia logica qui...
+}
+*/
+
+// --- NUOVO CODICE (Attivo, viene eseguito) ---
+const titolo = ref("Catamarano Luxury")
+
+function cambiaTitolo() {
+  titolo.value = "Catamarano Luxury - Winter Edition"
+}
+</script>
+
+
+
+
+
+
+
+
+<style scoped>
+/* Qui inizieremo a scrivere il CSS */
+h1 {
+  color: #0077b6; /* Un bel blu mare */
+}
+</style>
+
